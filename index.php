9e84d3146093b1539276f550f6e9e83ab5964b6d
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-10-08 13:38:50 +0200

    livreOR: création page d'accueil index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Livre d'or</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Livre d'or</h1>
+    <a href="inscription.php">Inscription</a>
+    <a href="connexion.php">Connexion</a>
+    <a href="livre-or.php">Voir le livre d'or</a>
+</body>
+</html>
