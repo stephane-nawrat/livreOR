799f7faecbcdb2c6e5539856142db6b2ff568108
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-10-08 15:59:58 +0200

    livreOR: ajout base style.css

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Livre d'or</h1>
-    <a href="inscription.php">Inscription</a>
-    <a href="connexion.php">Connexion</a>
-    <a href="livre-or.php">Voir le livre d'or</a>
+    <header>
+        <h1>Livre d'or</h1>
+        <nav>
+            <a href="inscription.php">Inscription</a>
+            <a href="connexion.php">Connexion</a>
+            <a href="livre-or.php">Voir le livre d'or</a>
+        </nav>
+    </header>
+    <main>
+        <h2>Bienvenue sur le Livre d'or</h2>
+        <p>Partagez vos avis et commentaires ici.</p>
+    </main>
 </body>
 </html>
